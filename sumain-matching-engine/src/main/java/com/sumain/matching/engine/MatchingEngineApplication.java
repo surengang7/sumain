34088cf3d89ae8e1d1fb923263781ece9abcd772
surengang <surengang@hqshuke.com>
2025-05-27 16:34:35 +0800

    enable async

--- sumain-matching-engine/src/main/java/com/sumain/matching/engine/MatchingEngineApplication.java
+++ sumain-matching-engine/src/main/java/com/sumain/matching/engine/MatchingEngineApplication.java
@@ -3,8 +3,10 @@ package com.sumain.matching.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MatchingEngineApplication {
 
     public static void main(String[] args) {
