c2cc306c6f8244c02846146893791387ef0e1e2c
surengang <surengang@hqshuke.com>
2025-08-05 18:04:19 +0800

    add time duration class

---
+++ sumain-common/src/main/java/com/sumain/common/entity/TimeDuration.java
@@ -0,0 +1,32 @@
+package com.sumain.common.entity;
+
+
+import com.sumain.common.utils.DateUtils;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@Data
+@Builder
+public class TimeDuration {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Long duration;
+
+    public Long getDuration() {
+        return Duration.between(this.startTime, this.endTime).toMillis();
+    }
+
+    public boolean isWithin(String timeStr) {
+        LocalTime localTime = DateUtils.toLocalTime(timeStr);
+        return isWithin(localTime);
+    }
+
+    public boolean isWithin(LocalTime time) {
+        return (time.equals(startTime) || time.isAfter(startTime)) && time.isBefore(endTime);
+    }
+
+
+}
